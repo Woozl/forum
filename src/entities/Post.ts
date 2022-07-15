d032f935175ab72aa0587d2371fe50b4d6da5902
David Glymph <Woozl@users.noreply.github.com>
2022-07-15 18:19:47 -0400

    prettier init

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -1,13 +1,13 @@
-import { Entity, OptionalProps, PrimaryKey, Property } from "@mikro-orm/core";
+import { Entity, OptionalProps, PrimaryKey, Property } from '@mikro-orm/core';
 
 @Entity()
 export class Post {
-  [OptionalProps]?: 'updatedAt' | 'createdAt'; 
+  [OptionalProps]?: 'updatedAt' | 'createdAt';
 
   @PrimaryKey()
   id!: number;
 
-  @Property({type: 'date'})
+  @Property({ type: 'date' })
   createdAt = new Date();
 
   @Property({ type: 'date', onUpdate: () => new Date() })
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
-import { MikroORM } from "@mikro-orm/core";
-import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
-import mikroOrmConfig from "./mikro-orm.config";
+import { MikroORM } from '@mikro-orm/core';
+import { __prod__ } from './constants';
+import { Post } from './entities/Post';
+import mikroOrmConfig from './mikro-orm.config';
 
 const main = async () => {
-    const orm = await MikroORM.init(mikroOrmConfig);
+  const orm = await MikroORM.init(mikroOrmConfig);
 
-    await orm.getMigrator().up();
+  await orm.getMigrator().up();
 
-    // const post = orm.em.create(Post, { title: 'Test' });
-    // await orm.em.persistAndFlush(post);
+  // const post = orm.em.create(Post, { title: 'Test' });
+  // await orm.em.persistAndFlush(post);
 
-    const posts = await orm.em.find(Post, {});
-    console.log(posts);
-}
+  const posts = await orm.em.find(Post, {});
+  console.log(posts);
+};
 
 main();
