ebd732a8456f8960853641d36255a09052ba106c
David Glymph <Woozl@users.noreply.github.com>
2022-07-17 09:21:57 -0400

    store user session on registration

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -51,7 +51,7 @@ export class UserResolver {
   @Mutation(() => UserResponse)
   async register(
     @Arg('options') options: UsernamePasswordInput,
-    @Ctx() { em }: MyContext
+    @Ctx() { em, req }: MyContext
   ): Promise<UserResponse> {
     if (options.username.length <= 2)
       return {
@@ -87,6 +87,8 @@ export class UserResolver {
           errors: [{ field: 'username', message: 'Username already exists.' }]
         };
     }
+
+    req.session.userId = user.id;
 
     return { user };
   }
