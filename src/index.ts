a8d6f5e391625f41c08a36f11b5f902d58dd21f5
David Glymph <Woozl@users.noreply.github.com>
2022-07-17 09:21:06 -0400

    remove localhost:4000 from cors origin

--- src/index.ts
+++ src/index.ts
@@ -59,11 +59,7 @@ const main = async () => {
   apolloServer.applyMiddleware({
     app,
     cors: {
-      origin: [
-        'https://studio.apollographql.com',
-        'http://localhost:4000/graphql',
-        'https://localhost:3000'
-      ],
+      origin: ['https://studio.apollographql.com', 'https://localhost:3000'],
       credentials: true
     }
   });
